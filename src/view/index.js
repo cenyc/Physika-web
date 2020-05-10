27dc7d57a9e9a107d284735dc00d076091f0d37b
cenyc <yccen@m.scnu.edu.cn>
2020-05-11 01:32:21 +0800

    新增载入模型功能

--- src/view/index.js
+++ src/view/index.js
@@ -17,6 +17,7 @@ import vtkFullScreenRenderWindow from 'vtk.js/Sources/Rendering/Misc/FullScreenR
 import vtkOBJReader from 'vtk.js/Sources/IO/Misc/OBJReader';
 import vtkActor from "vtk.js/Sources/Rendering/Core/Actor";
 import vtkMapper from 'vtk.js/Sources/Rendering/Core/Mapper';
+import file from "paraviewweb/src/IO/Girder/CoreEndpoints/file";
 
 // require('bootstrap/dist/css/bootstrap.custom.min.css');
 // var React = require('react');
@@ -56,170 +57,64 @@ class LeftNav extends Component {
 class GeoViewer extends Component{
     render() {
         return <div id="content">
-            <nav className="navbar navbar-light navbar-expand bg-white shadow mb-4 topbar static-top">
-                <div className="container-fluid">
-                    <button className="btn btn-link d-md-none rounded-circle mr-3" id="sidebarToggleTop" type="button">
-                        <i className="fas fa-bars"></i></button>
-                    <form
-                        className="form-inline d-none d-sm-inline-block mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
-                        <div className="input-group"><input className="bg-light form-control border-0 small" type="text"
-                                                            placeholder="Search for ..."/>
-                            <div className="input-group-append">
-                                <button className="btn btn-primary py-0" type="button"><i className="fas fa-search"></i>
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                    <ul className="nav navbar-nav flex-nowrap ml-auto">
-                        <li className="nav-item dropdown d-sm-none no-arrow"><a className="dropdown-toggle nav-link"
-                                                                                data-toggle="dropdown"
-                                                                                aria-expanded="false" href="#"><i
-                            className="fas fa-search"></i></a>
-                            <div className="dropdown-menu dropdown-menu-right p-3 animated--grow-in" role="menu"
-                                 aria-labelledby="searchDropdown">
-                                <form className="form-inline mr-auto navbar-search w-100">
-                                    <div className="input-group"><input className="bg-light form-control border-0 small"
-                                                                        type="text" placeholder="Search for ..."/>
-                                        <div className="input-group-append">
-                                            <button className="btn btn-primary py-0" type="button"><i
-                                                className="fas fa-search"></i></button>
-                                        </div>
-                                    </div>
-                                </form>
-                            </div>
-                        </li>
-                        <li className="nav-item dropdown no-arrow mx-1" role="presentation">
-                            <div className="nav-item dropdown no-arrow"><a className="dropdown-toggle nav-link"
-                                                                           data-toggle="dropdown" aria-expanded="false"
-                                                                           href="#"><span
-                                className="badge badge-danger badge-counter">3+</span><i
-                                className="fas fa-bell fa-fw"></i></a>
-                                <div
-                                    className="dropdown-menu dropdown-menu-right dropdown-list dropdown-menu-right animated--grow-in"
-                                    role="menu">
-                                    <h6 className="dropdown-header">alerts center</h6>
-                                    <a className="d-flex align-items-center dropdown-item" href="#">
-                                        <div className="mr-3">
-                                            <div className="bg-primary icon-circle"><i
-                                                className="fas fa-file-alt text-white"></i></div>
-                                        </div>
-                                        <div><span className="small text-gray-500">December 12, 2019</span>
-                                            <p>A new monthly report is ready to download!</p>
-                                        </div>
-                                    </a>
-                                    <a className="d-flex align-items-center dropdown-item" href="#">
-                                        <div className="mr-3">
-                                            <div className="bg-success icon-circle"><i
-                                                className="fas fa-donate text-white"></i></div>
-                                        </div>
-                                        <div><span className="small text-gray-500">December 7, 2019</span>
-                                            <p>$290.29 has been deposited into your account!</p>
-                                        </div>
-                                    </a>
-                                    <a className="d-flex align-items-center dropdown-item" href="#">
-                                        <div className="mr-3">
-                                            <div className="bg-warning icon-circle"><i
-                                                className="fas fa-exclamation-triangle text-white"></i></div>
-                                        </div>
-                                        <div><span className="small text-gray-500">December 2, 2019</span>
-                                            <p>Spending Alert: We&#39;ve noticed unusually high spending for your
-                                                account.</p>
-                                        </div>
-                                    </a><a className="text-center dropdown-item small text-gray-500" href="#">Show All
-                                    Alerts</a></div>
-                            </div>
-                        </li>
-                        <li className="nav-item dropdown no-arrow mx-1" role="presentation">
-                            <div className="nav-item dropdown no-arrow"><a className="dropdown-toggle nav-link"
-                                                                           data-toggle="dropdown" aria-expanded="false"
-                                                                           href="#"><i
-                                className="fas fa-envelope fa-fw"></i><span
-                                className="badge badge-danger badge-counter">7</span></a>
-                                <div
-                                    className="dropdown-menu dropdown-menu-right dropdown-list dropdown-menu-right animated--grow-in"
-                                    role="menu">
-                                    <h6 className="dropdown-header">alerts center</h6>
-                                    <a className="d-flex align-items-center dropdown-item" href="#">
-                                        <div className="dropdown-list-image mr-3"><img className="rounded-circle"
-                                                                                       src="avatars/avatar4.jpeg"/>
-                                            <div className="bg-success status-indicator"></div>
-                                        </div>
-                                        <div className="font-weight-bold">
-                                            <div className="text-truncate"><span>Hi there! I am wondering if you can help me with a problem I&#39;ve been having.</span>
-                                            </div>
-                                            <p className="small text-gray-500 mb-0">Emily Fowler - 58m</p>
-                                        </div>
-                                    </a>
-                                    <a className="d-flex align-items-center dropdown-item" href="#">
-                                        <div className="dropdown-list-image mr-3"><img className="rounded-circle"
-                                                                                       src="avatars/avatar2.jpeg"/>
-                                            <div className="status-indicator"></div>
-                                        </div>
-                                        <div className="font-weight-bold">
-                                            <div className="text-truncate"><span>I have the photos that you ordered last month!</span>
-                                            </div>
-                                            <p className="small text-gray-500 mb-0">Jae Chun - 1d</p>
-                                        </div>
-                                    </a>
-                                    <a className="d-flex align-items-center dropdown-item" href="#">
-                                        <div className="dropdown-list-image mr-3"><img className="rounded-circle"
-                                                                                       src="avatars/avatar3.jpeg"/>
-                                            <div className="bg-warning status-indicator"></div>
-                                        </div>
-                                        <div className="font-weight-bold">
-                                            <div className="text-truncate"><span>Last month&#39;s report looks great, I am very happy with the progress so far, keep up the good work!</span>
-                                            </div>
-                                            <p className="small text-gray-500 mb-0">Morgan Alvarez - 2d</p>
-                                        </div>
-                                    </a>
-                                    <a className="d-flex align-items-center dropdown-item" href="#">
-                                        <div className="dropdown-list-image mr-3"><img className="rounded-circle"
-                                                                                       src="avatars/avatar5.jpeg"/>
-                                            <div className="bg-success status-indicator"></div>
-                                        </div>
-                                        <div className="font-weight-bold">
-                                            <div className="text-truncate"><span>Am I a good boy? The reason I ask is because someone told me that people say this to all dogs, even if they aren&#39;t good...</span>
-                                            </div>
-                                            <p className="small text-gray-500 mb-0">Chicken the Dog · 2w</p>
-                                        </div>
-                                    </a><a className="text-center dropdown-item small text-gray-500" href="#">Show All
-                                    Alerts</a></div>
-                            </div>
-                            <div className="shadow dropdown-list dropdown-menu dropdown-menu-right"
-                                 aria-labelledby="alertsDropdown"></div>
-                        </li>
-                        <div className="d-none d-sm-block topbar-divider"></div>
-                        <li className="nav-item dropdown no-arrow" role="presentation">
-                            <div className="nav-item dropdown no-arrow"><a className="dropdown-toggle nav-link"
-                                                                           data-toggle="dropdown" aria-expanded="false"
-                                                                           href="#"><span
-                                className="d-none d-lg-inline mr-2 text-gray-600 small">Valerie Luna</span><img
-                                className="border rounded-circle img-profile" src="avatars/avatar1.jpeg"/></a>
-                                <div
-                                    className="dropdown-menu shadow dropdown-menu-right animated--grow-in" role="menu">
-                                    <a className="dropdown-item" role="presentation" href="#"><i
-                                        className="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i> Profile</a><a
-                                    className="dropdown-item" role="presentation" href="#"><i
-                                    className="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i> Settings</a>
-                                    <a
-                                        className="dropdown-item" role="presentation" href="#"><i
-                                        className="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i> Activity log</a>
-                                    <div className="dropdown-divider"></div>
-                                    <a className="dropdown-item" role="presentation" href="#"><i
-                                        className="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i> Logout</a>
-                                </div>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-            <div className="container-fluid" id={"geoViewer"}>
+            <div className="container-fluid p-0" id={"geoViewer"}>
 
             </div>
         </div>;
     }
 }
 
+/**
+ * 加载模型响应事件
+ * @param event
+ * @param fullScreenRenderer
+ */
+function input_geo_file_handle(event, fullScreenRenderer) {
+    event.preventDefault();
+    const geo_file = event.target.files;
+
+    if (geo_file.length == 1) {
+        const ext = geo_file[0].name.split('.').slice(-1)[0];
+        console.log('loading geometry file successfully, is name '+geo_file[0].name+' and ext is '+ext+'.');
+        load(fullScreenRenderer, {file: geo_file[0], ext});
+    }
+}
+
+/**
+ * 加载显示几何体
+ * @param options
+ */
+function load(fullScreenRenderer, options) {
+    const renderer = fullScreenRenderer.getRenderer();
+    const renderWindow = fullScreenRenderer.getRenderWindow();
+    // 加载obj
+    if (options.file && options.ext === 'obj') {
+        console.log('loading obj... '+options.file.name);
+        const reader = new FileReader();
+        reader.onload = function (event) {
+            const objReader = vtkOBJReader.newInstance();
+            objReader.parseAsText(reader.result);
+            const nbOutputs = objReader.getNumberOfOutputPorts();
+            console.log('nbOutputs is '+nbOutputs);
+            for (let idx = 0; idx < nbOutputs; idx++) {
+                const source = objReader.getOutputData(idx);
+                const mapper = vtkMapper.newInstance();
+                const actor = vtkActor.newInstance();
+                actor.setMapper(mapper);
+                mapper.setInputData(source);
+                renderer.addActor(actor);
+            }
+            console.log('rendering geo...'+options.file.name)
+            renderer.resetCamera();
+            renderWindow.render();
+        };
+        reader.readAsText(options.file);
+    }
+}
+
+/**
+ * 首页初始化
+ */
 function init() {
     window.onload = function() {
         //首页左边布局
@@ -238,26 +133,35 @@ function init() {
             rootContainer: geoViewer,
             containerStyle: { height: '100%', width: '100%', position: 'absolute' },
         });
-        const renderer = fullScreenRenderer.getRenderer();
-        const renderWindow = fullScreenRenderer.getRenderWindow();
+        // const renderer = fullScreenRenderer.getRenderer();
+        // const renderWindow = fullScreenRenderer.getRenderWindow();
 
-        const objReader = vtkOBJReader.newInstance();
+        // const objReader = vtkOBJReader.newInstance();
+        //
+        // objReader.setUrl('/static/geo/mujia.obj').then(() => {
+        //     console.log(objReader.getNumberOfOutputPorts());
+        //
+        //     const source = objReader.getOutputData(0);
+        //     const mapper = vtkMapper.newInstance();
+        //     const actor = vtkActor.newInstance();
+        //
+        //     actor.setMapper(mapper);
+        //     mapper.setInputData(source);
+        //     renderer.addActor(actor);
+        //
+        //     renderer.resetCamera();
+        //     renderWindow.render();
+        // });
 
-        objReader.setUrl('/static/geo/mujia.obj').then(() => {
-            console.log(objReader.getNumberOfOutputPorts());
-
-            const source = objReader.getOutputData(0);
-            const mapper = vtkMapper.newInstance();
-            const actor = vtkActor.newInstance();
-
-            actor.setMapper(mapper);
-            mapper.setInputData(source);
-            renderer.addActor(actor);
-
-            renderer.resetCamera();
-            renderWindow.render();
+        let sq_btn = document.getElementById('sidebarToggle');
+        sq_btn.innerHTML = `<input type="file" accept=".zip,.obj" style="display: none;"/>`;
+        let input_geo_file = sq_btn.querySelector('input');
+        input_geo_file.addEventListener('change', function (event) {
+            if (!event)
+                event = window.event;
+            input_geo_file_handle(event, fullScreenRenderer)
         });
-
+        sq_btn.addEventListener('click', (e) => input_geo_file.click());
 
         // objReader.readAsText('/static/geo/mujia.obj');
         // fileReader.onload = function(event) {
@@ -266,6 +170,8 @@ function init() {
         // fileReader.readAsText('/static/geo/mujia.obj');
     }
 }
+
+
 
 export {init}
 
