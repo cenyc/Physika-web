0e0e2aa1088b626f7236da64dfeed2c3d2780cca
liyunfei18 <544642461@qq.com>
2021-01-08 21:59:59 +0800

    。。。

--- src_client/View/Cloth/index.js
+++ src_client/View/Cloth/index.js
@@ -1,21 +1,23 @@
-import 'bootstrap';
 import React from 'react';
-import { Tree, Button } from 'antd';
-import { BiShow, BiHide, BiPointer, BiMinus } from 'react-icons/bi'
+import { Tree, Button, Divider, Descriptions, Collapse} from 'antd';
 const { TreeNode } = Tree;
+const { Panel } = Collapse;
 //antd样式
 import 'antd/dist/antd.css';
 //渲染窗口
 import vtkFullScreenRenderWindow from 'vtk.js/Sources/Rendering/Misc/FullScreenRenderWindow';
 //面片拾取
-import vtkCellPicker from 'vtk.js/Sources/Rendering/Core/CellPicker';
 import { physikaLoadConfig } from '../../IO/LoadConfig'
 import { physikaUploadConfig } from '../../IO/UploadConfig'
 import { PhysikaTreeNodeAttrModal } from '../TreeNodeAttrModal'
-import { physikaLoadObj } from '../../IO/LoadObj';
+import { physikaInitObj } from '../../IO/InitObj';
 import { getOrientationMarkerWidget } from '../Widget/OrientationMarkerWidget';
+import { parseSimulationResult } from '../../Common'
 
-const simtype=1;
+import WebworkerPromise from 'webworker-promise';
+import WSWorker from '../../Worker/ws.worker';
+
+const simtype = 1;
 
 class ClothSimulation extends React.Component {
     constructor(props) {
@@ -31,7 +33,6 @@ class ClothSimulation extends React.Component {
 
             isTreeNodeAttrModalShow: false,
             uploadDisabled: true,
-            isSliderShow: false,
         };
     }
 
@@ -76,9 +77,7 @@ class ClothSimulation extends React.Component {
         this.renderWindow.render();
 
         this.setState({
-            description: [],
-            animation: false,
-            isSliderShow: false,
+            description: []
         });
     }
 
@@ -111,6 +110,7 @@ class ClothSimulation extends React.Component {
         this.renderWindow.render();
     }
 
+    /*
     //改变actor的可见性
     changeVisible = (item) => {
         const actor = this.curScene[item._attributes.name].actor;
@@ -121,26 +121,13 @@ class ClothSimulation extends React.Component {
         this.forceUpdate();
         this.renderWindow.render();
     }
+    */
 
     //递归渲染每个树节点（这里必须用map遍历！可能因为需要返回的数组？）
     renderTreeNodes = (data) => data.map((item, index) => {
         item.title = (
             <div>
-                {
-                    (item.tag === 'Node') &&
-                    (this.curScene[item._attributes.name].actor.getVisibility()
-                        ? <BiShow onClick={() => this.changeVisible(item)}></BiShow>
-                        : <BiHide onClick={() => this.changeVisible(item)}></BiHide>)
-                }
                 <Button type="text" size="small" onClick={() => this.showTreeNodeAttrModal(item)}>{item._attributes.name}</Button>
-                {
-                    (item.tag === 'Node') &&
-                    <BiPointer onClick={() => this.cellPick(item)}></BiPointer>
-                }
-                {
-                    (item.tag === 'Cell') &&
-                    <BiMinus onClick={() => this.deleteNode(item.key)}></BiMinus>
-                }
             </div>
         );
 
@@ -195,155 +182,83 @@ class ClothSimulation extends React.Component {
     }
 
     upload = () => {
-        if (this.state.data.length == 0) {
-            alert("还未加载配置文件！");
-        }
-        else {
-            //第一个参数data，第二个参数仿真类型
-            physikaUploadConfig(this.state.data, 'cloth')
+        this.clean();
+        this.setState({
+            uploadDisabled: true,
+        }, () => {
+            physikaUploadConfig(this.state.data, simtype)
                 .then(res => {
                     console.log("成功上传配置并获取到仿真结果配置");
-                    let options = this.extractURL(res);
-                    return Promise.all([physikaLoadObj(options), res]);
+                    const resultInfo = parseSimulationResult(res);
+                    this.fileName = resultInfo.fileName;
+                    this.frameSum = resultInfo.frameSum;
+                    this.setState({
+                        description: resultInfo.description,
+                    });
+                    if (this.frameSum > 0) {
+                        return this.wsWorker.postMessage({
+                            data: { fileName: this.fileName + '.obj' }
+                        });
+                    }
+                    else {
+                        return Promise.reject('模拟帧数不大于0！');
+                    }
                 })
                 .then(res => {
-                    console.log("成功获取仿真结果模型", res);
-                    this.frameSeq = res[0];
-                    this.updateScene(this.frameSeq[0]);
-                    this.setState({ data: res[1] });
+                    //注意后缀！
+                    return physikaInitObj(res, 'zip');
+                })
+                .then(res => {
+                    this.updateScene(res);
+                    //显示方向标记部件
+                    this.orientationMarkerWidget.setEnabled(true);
+                    this.setState({
+                        uploadDisabled: false,
+                    });
                 })
                 .catch(err => {
                     console.log("Error uploading: ", err);
-                });
-        }
-    }
-
-    //---------2020.10.15 面片选取----------
-    cellPick = (item) => {
-        console.log("开始选取");
-
-        const picker = vtkCellPicker.newInstance();
-        picker.setPickFromList(true);
-        picker.setTolerance(0);
-        picker.initializePickList();
-        picker.addPickList(this.curScene[item._attributes.name].actor);
-        //console.log(picker.getPickList());
-
-        const subscription = this.renderWindow.getInteractor().onRightButtonPress((callData) => {
-            if (this.renderer !== callData.pokedRenderer) {
-                return;
-            }
-
-            const pos = callData.position;
-            const point = [pos.x, pos.y, 0.0];
-            console.log(`Pick at: ${point}`);
-            picker.pick(point, this.renderer);
-
-            if (picker.getActors().length === 0) {
-                const pickedPoint = picker.getPickPosition();
-                console.log(`No cells picked, default: ${pickedPoint}`);
-            }
-            else {
-                const pickedCellId = picker.getCellId();
-                console.log('Picked cell: ', pickedCellId);
-                const pickedPoints = picker.getPickedPositions();
-                for (let i = 0; i < pickedPoints.length; i++) {
-                    const pickedPoint = pickedPoints[i];
-                    console.log(`Picked: ${pickedPoint}`);
-                }
-                //添加选定节点
-                this.showTreeNodeAttrModal(addPickedCell(pickedCellId, item));
-            }
-            //取消鼠标右键点击的订阅（2020.10.17）
-            subscription.unsubscribe();
+                })
         });
     }
-    //------------------------------------
 
-    //删除指定树节点（其父节点保留）
-    deleteNode = (treeNodeKey) => {
-        let eachKey = treeNodeKey.split('-');
-        let count = 0;
-        const findTreeNodeKey = (node) => {
-            //找到treeNodeKey所在的children数组
-            if (count === eachKey.length - 1) {
-                //删除对应节点
-                node.splice(eachKey[count], 1);
-                //父节点key值变化，所以其所有子结点的key值也需要更新！
-                const changeNodeKey = (node_, fatherNodeKey) =>
-                    node_.forEach((item, index) => {
-                        item.key = fatherNodeKey + index;
-                        if (item.hasOwnProperty('children')) {
-                            changeNodeKey(item.children, item.key + '-');
-                        }
-                    });
-                changeNodeKey(node, treeNodeKey.slice(0, treeNodeKey.length - 1));
-                return;
-            }
-            findTreeNodeKey(node[eachKey[count++]].children);
-        };
-        findTreeNodeKey(this.state.data);
-        this.forceUpdate();
-    }
-    //------------------------------------
-
-    //2020.11.9 实现动画
-    drawFrame = () => {
-        const currentTime = new Date().getTime();
-        if (this.lastTime === 0) {
-            this.lastTime = currentTime;
-        }
-        const elapsed = currentTime - this.lastTime;
-        if (elapsed > (1000 / this.fps)) {
-            this.lastTime = currentTime - (elapsed % (1000 / this.fps));
-            if (this.frameSeqIndex === this.frameSeq.length) {
-                cancelAnimationFrame(this.rAF);
-                this.frameSeqIndex = 0;
-                console.log("动画结束");
-                return;
-            }
-            this.updateScene(this.frameSeq[this.frameSeqIndex]);
-            this.frameSeqIndex++;
-        }
-        this.rAF = requestAnimationFrame(this.drawFrame);
-    }
-
-    stopDrawFrame = () => {
-        cancelAnimationFrame(this.rAF);
-        console.log("动画暂停");
-    }
-    //-----------------------------------
+    renderDescriptions = () => this.state.description.map((item, index) => {
+        return <Descriptions.Item label={item.name} key={index}>{item.content}</Descriptions.Item>
+    })
 
     render() {
         console.log("tree:", this.state.data);
         return (
-            <div className="w-100">
-                <div className="card border rounded-0"><span className="text-center m-1">几何展示</span>
-                    <hr className="m-0" />
-                    <div className="card-body pt-2">
-                        <button className="btn btn-danger btn-sm p-0 btn-block" type="button" onClick={this.load}><span className="glyphicon glyphicon-plus">加载场景</span></button>
-                        <div className="pt-2" style={{ overflowY: 'auto', height: '333px' }}>
-                            <Tree style={{ overflowX: 'auto', width: '176px' }}>
-                                {this.renderTreeNodes(this.state.data)}
-                            </Tree>
-                        </div>
-
-                        <button className="btn btn-danger btn-sm p-0 btn-block" type="button" onClick={this.upload}><span className="glyphicon glyphicon-plus">执行仿真</span></button>
-                        <Button type="text" size="small" onClick={this.drawFrame}>播放动画</Button>
-                        <Button type="text" size="small" onClick={this.stopDrawFrame}>暂停动画</Button>
-                    </div>
-                    <div >
-                        <PhysikaTreeNodeAttrModal
-                            treeNodeAttr={this.state.treeNodeAttr}
-                            treeNodeText={this.state.treeNodeText}
-                            visible={this.state.isTreeNodeAttrModalShow}
-                            hideModal={this.hideTreeNodeAttrModal}
-                            changeData={(obj) => this.changeData(obj)}
-                        ></PhysikaTreeNodeAttrModal>
-                    </div>
+            <div>
+                <Divider>单张图像构建三维云</Divider>
+                <Collapse defaultActiveKey={['1']}>
+                    <Panel header="仿真初始化" key="1">
+                        <Button type="primary" size={'small'} block onClick={this.load}>加载场景</Button>
+                        <Tree style={{ overflowX: 'auto', width: '200px' }}>
+                            {this.renderTreeNodes(this.state.data)}
+                        </Tree>
+                        <br />
+                        <Button type="primary" size={'small'} block onClick={this.upload} disabled={this.state.uploadDisabled}>开始仿真</Button>
+                    </Panel>
+                    <Panel header="仿真结果信息" key="2">
+                        <Descriptions column={1} layout={'horizontal'}>
+                            {this.renderDescriptions()}
+                        </Descriptions>
+                    </Panel>
+                    <Panel header="仿真展示控制" key="3">
+                    </Panel>
+                </Collapse>
+                <div>
+                    <PhysikaTreeNodeAttrModal
+                        treeNodeAttr={this.state.treeNodeAttr}
+                        treeNodeText={this.state.treeNodeText}
+                        visible={this.state.isTreeNodeAttrModalShow}
+                        hideModal={this.hideTreeNodeAttrModal}
+                        changeData={(obj) => this.changeData(obj)}
+                    ></PhysikaTreeNodeAttrModal>
                 </div>
             </div>
-        );
+        )
     }
 }
 
